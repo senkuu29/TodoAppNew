1b6fd8f0ab351c9a4bb6bbf32ae52a515098d58d
senkuu29 <2223590.deden@smkn-2sbg.sch.id>
2025-02-12 12:54:13 +0700

    penambahan route taskController

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,7 @@ use App\Models\Task;
 // Resource route untuk TaskListController
 // Ini secara otomatis membuat route untuk operasi CRUD pada daftar tugas (lists)
 Route::resource('lists', TaskListController::class);
+
+// Resource route untuk TaskController
+// Ini secara otomatis membuat route untuk operasi CRUD pada tugas (tasks)
+Route::resource('tasks', TaskController::class);
