41d66dad9baef2453706c8901e81e8f1f2aab037
senkuu29 <2223590.deden@smkn-2sbg.sch.id>
2025-02-12 10:46:53 +0700

    menambahkan methood  di model\task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,6 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     
+    //  untuk menentukan atribut mana yang boleh diisi secara massal (mass assignment).
+    protected $fillable = [
+        'name',
+        'description',
+        'is_completed',
+        'priority',
+        'list_id'
+    ];
+
      // many-to-one (antara model Task dan model TaskList)
      public function list() {
         return $this->belongsTo(TaskList::class, 'list_id');
