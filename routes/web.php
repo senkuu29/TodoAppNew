d0c5c0615477ad46f8e92dc8534ac24d790997b0
senkuu29 <2223590.deden@smkn-2sbg.sch.id>
2025-02-12 12:49:49 +0700

    penambahan route tasklistcontroller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TaskListController;
 use Illuminate\Support\Facades\Route;
+use App\Models\Task;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Resource route untuk TaskListController
+// Ini secara otomatis membuat route untuk operasi CRUD pada daftar tugas (lists)
+Route::resource('lists', TaskListController::class);
