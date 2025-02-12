0b9915efafb5f4b2b98c5755b72b668828c05bbd
senkuu29 <2223590.deden@smkn-2sbg.sch.id>
2025-02-12 10:52:32 +0700

    penambahan methood guarded di models\tasklists

--- app/Models/TaskList.php
+++ app/Models/TaskList.php
@@ -8,7 +8,13 @@ class TaskList extends Model
 {
     
     protected $fillable = ['name'];
-    
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at'
+    ];
+
      // many-to-many (antara model TaskList dan model Task)
     public function tasks() {
         return $this->hasMany(Task::class, 'list_id');
