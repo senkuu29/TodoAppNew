949ff8ac1905deb2500eae919fddef3ba4f3564e
senkuu29 <2223590.deden@smkn-2sbg.sch.id>
2025-02-12 11:39:39 +0700

    modified pemanggilan modelnya dan penambahan data seed di seeder\taskseeder

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -1,7 +1,10 @@
 <?php
 
+
 namespace Database\Seeders;
 
+use App\Models\Task;
+use App\Models\TaskList;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +15,45 @@ class TaskSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $tasks = [
+            [
+                'name' => 'Papandayan',
+                'description' => 'Sebelum masuk Semester Genap Kelas Dua Belas RPL',
+                'is_completed' => true,
+                'priority' => 'medium',
+                'list_id' => TaskList::where('name', 'MDPL')->first()->id,
+            ],
+            [
+                'name' => 'Gn. Putri',
+                'description' => 'Sesudah menjelang masuk Semester Ganjil Dua Belas RPL',
+                'is_completed' => false,
+                'priority' => 'medium',
+                'list_id' => TaskList::where('name', 'MDPL')->first()->id,
+            ],
+            [
+                'name' => 'Gn. Canggah',
+                'description' => 'sesudah beres Sekolah',
+                'is_completed' => false,
+                'priority' => 'medium',
+                'list_id' => TaskList::where('name', 'MDPL')->first()->id,
+            ],
+            [
+                'name' => 'Gn. Gede',
+                'description' => 'sesudah beres Sekolah',
+                'is_completed' => false,
+                'priority' => 'medium',
+                'list_id' => TaskList::where('name', 'MDPL')->first()->id,
+            ],
+            [
+                'name' => 'Gn. Ciremai',
+                'description' => 'sesudah lebaran kita gas hiking bersama Tutut, Feri, Tami, Elgin dan Ikhsan',
+                'is_completed' => false,
+                'priority' => 'medium',
+                'list_id' => TaskList::where('name', 'MDPL')->first()->id,
+            ],
+            
+        ];
+
+        Task::insert($tasks);
     }
 }
