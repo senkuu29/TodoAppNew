252a3527f1cd3c3c0de33ed59585410d8ea3933d
senkuu29 <2223590.deden@smkn-2sbg.sch.id>
2025-02-13 14:29:40 +0700

    penambahan bgHero beserta text berjalan

--- resources/views/pages/details.blade.php
+++ resources/views/pages/details.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app-details')
 
 @section('content')
-    <h1>test</h1>
+{{-- background gambar beserta tulisan berjalan --}}
+<div class="hero text-center py-5">
+    <h1 class="typing-animation">Create Your Daily List Here</h1>
+</div>
 @endsection
 
