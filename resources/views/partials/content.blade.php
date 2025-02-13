01a6b6cac4f63acb2c8840f6eef7595f5d519b65
senkuu29 <2223590.deden@smkn-2sbg.sch.id>
2025-02-13 11:24:51 +0700

    penambahan fitur card untuk menambahkan daftar list

--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -2,3 +2,30 @@
 <div class="hero text-center py-5">
     <h1 class="typing-animation">Create Your Daily List Here</h1>
 </div>
+
+<!-- Container utama -->
+<div class="container my-4">
+    <div class="row g-3">
+      
+         <!-- Kolom pertama: Kartu untuk menambahkan daftar list -->
+         <div class="col-md-4">
+            <div class="card shadow-sm h-100">
+                <div class="card-body">
+                    <h3 class="card-title">Buatlah List Favoritmu</h3>
+                    <hr>
+                    <p class="card-text">
+                        Kami menyediakan tempat koleksi list yang bisa kamu gunakan kapan saja.
+                    </p>
+                    
+                    <!-- Tombol untuk membuka modal tambah daftar list -->
+                    <button type="button" class="btn btn-primary btn-sm w-100 d-flex align-items-center justify-content-center gap-1 p-2 mt-3" 
+                        data-bs-toggle="modal" data-bs-target="#addListModal">
+                        <i class="bi bi-plus fs-5"></i> Tambah Daftar List
+                    </button>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</div>
+<hr>
