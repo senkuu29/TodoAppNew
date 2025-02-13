cc1b6a71e140d47647deac786417e65d613b1084
senkuu29 <2223590.deden@smkn-2sbg.sch.id>
2025-02-13 14:21:32 +0700

    penambahan modal penambah task

--- resources/views/partials/modal.blade.php
+++ resources/views/partials/modal.blade.php
@@ -22,3 +22,42 @@
         </form>
     </div>
 </div>
+
+{{-- modal task --}}
+<div class="modal fade" id="addTaskModal" tabindex="-1" aria-labelledby="addTaskModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <form action="{{ route('tasks.store') }}" method="POST" class="modal-content">
+            @method('POST')
+            @csrf
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="addTaskModalLabel">Tambah Task</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <input type="text" id="taskListId" name="list_id" hidden>
+                <div class="mb-3">
+                    <label for="name" class="form-label">Nama</label>
+                    <input type="text" class="form-control" id="name" name="name"
+                        placeholder="Masukkan nama list">
+                </div>
+                <div class="mb-3">
+                    <label for="name" class="form-label">Deskripsi</label>
+                    <input type="text" class="form-control" id="description" name="description"
+                        placeholder="Masukkan deskripsi">
+                </div>
+                <div class="mb-3">
+                    <label for="priority" class="form-label">Priority</label>
+                    <select class="form-select form  -select" aria-label="Small select example" id="priority" name="priority">
+                        <option value="low">low</option>
+                        <option value="medium" selected>medium</option>
+                        <option value="high">high</option>
+                    </select>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                <button type="submit" class="btn btn-primary">Tambah</button>
+            </div>
+        </form>
+    </div>
+</div>
