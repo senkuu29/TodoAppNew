4656d41860f3214364d54f89d717fdbe420393bd
senkuu29 <2223590.deden@smkn-2sbg.sch.id>
2025-02-13 08:49:46 +0700

    modified (penambahan calling bootstrap dan styleCss) appblade di views\layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    {{-- title --}}
+    <title>{{ $title }} - {{ config('app.name') }}</title>
+
+    {{-- style css --}}
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+     <!-- Bootstrap CSS -->
+     <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
+     <link rel="stylesheet" href="{{ asset('vendor/bootstrap-icons/font/bootstrap-icons.min.css') }}"> 
 </head>
 <body>
     
+
+      <!-- Bootstrap JS -->
+      <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+      <script src="{{ asset('js/script.js') }}"></script>
 </body>
 </html>
