3f89ced03f6e2297920dd42f28c0f4be167a60b1
senkuu29 <2223590.deden@smkn-2sbg.sch.id>
2025-02-13 14:34:44 +0700

    penambahan tombol kembali

--- resources/views/pages/details.blade.php
+++ resources/views/pages/details.blade.php
@@ -5,9 +5,14 @@
 <div class="hero text-center py-5">
     <h1 class="typing-animation">Create Your Daily List Here</h1>
 </div>
+
 <div id="content" class="p-4 bg-light shadow-sm rounded my-5">
     <div class="d-flex justify-content-between align-items-center border-bottom pb-2 mb-4">
         <h1 class="text-primary mb-0 align-items-center">Detail Tugas</h1>
+        {{-- tombol kembali ke home/halaman depan --}}
+        <a href="{{ route('home') }}" class="btn btn-outline-primary">
+            <i class="bi bi-arrow-left"></i> Kembali
+        </a>
     </div>
 </div>
 @endsection
