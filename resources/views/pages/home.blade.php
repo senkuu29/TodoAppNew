2f53614cea40d63db12ca119c766d6c0e451dbaa
senkuu29 <2223590.deden@smkn-2sbg.sch.id>
2025-02-13 13:36:41 +0700

    menambahkan button tambah task

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -11,6 +11,11 @@
                     <div class="card-header  d-flex justify-content-between align-items-center">
                         <h5 class="card-title">{{ $list->name }}</h5>  {{-- nama list --}}
                         <div class="w-25 d-flex gap-2">
+                            {{-- tombol tambah task --}}
+                            <button type="button" class="btn btn-sm btn-outline-primary d-flex align-items-center justify-content-conter gap-1"
+                                data-bs-toggle="modal" data-bs-target="#addTaskModal" data-list="{{ $list->id }}">
+                              <i class="bi bi-plus fs-6"></i> 
+                            </button>
                         </div>
                     </div>
                 </div>
