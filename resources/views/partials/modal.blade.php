05fcc420653f59e8f3d65e5f99f4724396d6badf
senkuu29 <2223590.deden@smkn-2sbg.sch.id>
2025-02-13 14:24:10 +0700

    penambahan modal edit

--- resources/views/partials/modal.blade.php
+++ resources/views/partials/modal.blade.php
@@ -61,3 +61,42 @@
         </form>
     </div>
 </div>
+
+{{-- fitur edit --}}
+<div class="modal fade" id="editTaskModal" tabindex="-1" aria-labelledby="editTaskModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <form id="editTaskForm" method="POST">
+            @method('PUT')
+            @csrf
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="editTaskModalLabel">Edit Task</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <input type="hidden" id="editTaskId" name="task_id">
+                    <div class="mb-3">
+                        <label for="editTaskName" class="form-label">Nama</label>
+                        <input type="text" class="form-control" id="editTaskName" name="name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="editTaskDescription" class="form-label">Deskripsi</label>
+                        <input type="text" class="form-control" id="editTaskDescription" name="description">
+                    </div>
+                    <div class="mb-3">
+                        <label for="editTaskPriority" class="form-label">Prioritas</label>
+                        <select class="form-select" id="editTaskPriority" name="priority">
+                            <option value="low">Low</option>
+                            <option value="medium">Medium</option>
+                            <option value="high">High</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                    <button type="submit" class="btn btn-primary">Simpan Perubahan</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
